16549fd0e1beff86f2365b1e2cfeea2f4a7c4b40
Krishna Kakadiya <krishna.nexios@gmail.com>
2024-04-25 15:30:57 +0530

    header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,31 @@
-import React from 'react'
+"use client"
+import Image from 'next/image';
+import React, { useEffect, useState } from 'react';
+import Logo from "../../assets/icons/Logo.svg"
 
 const Header = () => {
+  const [isSticky, setIsSticky] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const handleScroll = () => {
+    if (window.scrollY > 0) {
+      setIsSticky(true);
+    } else {
+      setIsSticky(false);
+    }
+  };
+
   return (
-    <div>
-      This is header
+    <div  className={`w-full h-[74px] py-5 px-10 bg-white ${
+      isSticky ? "sticky top-0  z-[9999] " : ""
+    } transition-all duration-500 shadow-lg`}>
+          <div className='flex gap-2'>
+              <Image src={Logo} width={50} height={40} ></Image>
+              <p className='font-inter font-normal text-[32px] text-black'>YieldMonk</p>
+          </div>
     </div>
   )
 }
